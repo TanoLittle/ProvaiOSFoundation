7031717ffb72500dda45cb43890b1625dab2082f
Tano <63610125+TanoLittle@users.noreply.github.com>
2023-02-18 17:09:03 +0100

    prova aggiunta nuovo file

---
+++ PrimoEsempio/ProvaAggiuntaFileSwitfIU.swift
@@ -0,0 +1,22 @@
+//
+//  ProvaAggiuntaFileSwitfIU.swift
+//  PrimoEsempio
+//
+//  Created by gaetanooliva on 18/02/23.
+//
+
+
+//in sud america
+import SwiftUI
+
+struct ProvaAggiuntaFileSwitfIU: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProvaAggiuntaFileSwitfIU_Previews: PreviewProvider {
+    static var previews: some View {
+        ProvaAggiuntaFileSwitfIU()
+    }
+}
